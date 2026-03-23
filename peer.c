5f289d602d4903807935026e6c723a89069ca949
Zenna Allwein <zrallwein@gmail.com>
2026-03-22 21:54:48 -0500

    created peer skeleton

--- peer.c
+++ peer.c
@@ -2,46 +2,203 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
 
+#define MAXLINE 100
+#define BACKLOG_LENGTH 256
 
+void read_client_thread_config(int* tracker_port, char* tracker_address, int* n_seconds) {
+    // read client thread config
+    FILE* client_thread_config;
+    client_thread_config = fopen("clientThreadConfig.cfg", "r");
+
+    // error if cannot find file
+    if(client_thread_config == NULL){
+        fprintf(stderr, "Could not open clientThreadConfig.cfg\n");
+        exit(1);
+    }
+
+    fscanf(client_thread_config, "%d %s %d", tracker_port, tracker_address, n_seconds);
+    fclose(client_thread_config);
+}
+
+void read_server_thread_config(int* server_port){
+    // read server thread config
+    FILE* server_thread_config;
+    server_thread_config = fopen("serverThreadConfig.cfg", "r");
+
+    // error if cannot find file
+    if(server_thread_config == NULL){
+        printf("Could not open serverThreadConfig.cfg\n");
+        exit(1);
+    }
+
+    fscanf(server_thread_config, "%d", server_port);
+    fclose(server_thread_config);
+}
+
+void peer_handler(int sock) {
+    // TODO: Fill this in
+}
+
+void start_server(int port) {
+    struct sockaddr_in server_addr, peer_addr;
+
+    int sock_child;
+    int sockid;
+
+    if ((sockid = socket(AF_INET,SOCK_STREAM,0)) < 0){ //create socket connection oriented
+        printf("socket cannot be created \n");
+        exit(1);
+    }
+
+    int opt = 1;
+    setsockopt(sockid, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = htons(INADDR_ANY);
+
+    // bind and check error
+    if (bind(sockid ,(struct sockaddr *) &server_addr,sizeof(server_addr)) ==-1){
+        printf("bind failure\n");
+        exit(1);
+    }
+
+    //(parent) process listens at sockid and check error
+    if (listen(sockid, BACKLOG_LENGTH) < 0){
+        printf("CANNOT LISTEN\n");
+        exit(1);
+    }
+
+    printf("LISTENING FOR INCOMING REQUESTS.... \n");
+
+    while(1) {
+        socklen_t peerlen = sizeof(peer_addr);
+        //wait until peer connects, then creates new socket (sock_child)
+        if ((sock_child = accept(sockid ,(struct sockaddr *) &peer_addr, &peerlen))==-1){
+        // accept connection and create a socket descriptor for actual work
+            printf("Cannot accept...\n");
+            //dont kill server on failure
+            continue;
+        }
+
+        //New child process will serve the requester client. separate child will serve separate client
+        if (fork()==0){
+            //child does not need listener
+            close(sockid);
+            peer_handler(sock_child);
+            close (sock_child);
+            // kill the process. child process all done with work
+            exit(0);
+        }
+
+        // parent all done with client, only child will communicate with that client from now
+        close(sock_child);
+    }
+}
+
+void handle_list_com(int tracker_sock){
+    char* req = "req list";
+
+    if((write(tracker_sock, req, strlen(req))) < 0){// inform the server of the list request
+        printf("Send_request  failure\\n");
+        exit(1);
+    }
+
+    char msg[256];
+
+    ssize_t n;
+    if((n = read(tracker_sock, msg, sizeof(msg))) < 0){// read what server has said
+        printf("Read  failure\\n");
+        exit(1);
+    }
+
+    fwrite(msg, 1, n, stdout);
+    fflush(stdout);
+}
+
+void handle_create_tracker_com(){
+    // TODO: create tracker
+}
+
+void handle_update_tracker_com(){
+    // TODO: update tracerk
+}
+
+void handle_get_com(){
+    // TODO: get command
+}
+
+void handle_command(char* str, int tracker_sock) {
+    char* command = strtok(str, " ");
+
+    if(strcmp(command, "list") == 0){
+        handle_list_com(tracker_sock);
+    } else if(strcmp(command, "create_tracker") == 0){
+        handle_create_tracker_com();
+    } else if(strcmp(command, "update_tracker") == 0){
+        handle_update_tracker_com();
+    } else if(strcmp(command, "get") == 0) {
+        handle_get_com();
+    } else {
+        printf("Unkown command: %s", command);
+    }
+}
+
+int connect_tracker_server(char* tracker_address, int tracker_port){
+    int sockid;
+    struct sockaddr_in tracker_addr;
+
+    //create socket
+    if ((sockid = socket(AF_INET,SOCK_STREAM,0))==-1){
+        printf("Socket cannot be created\\n");
+        exit(0);
+    }
+
+    tracker_addr.sin_family = AF_INET; //host byte order
+    tracker_addr.sin_port = htons(tracker_port); // convert to network byte order
+
+    inet_pton(AF_INET, tracker_address, &tracker_addr.sin_addr);
+
+    //connect and error check
+    if (connect(sockid ,(struct sockaddr *) &tracker_addr,sizeof(struct sockaddr))==-1){
+
+        printf("Cannot connect to tracker server\n");
+        exit(1);
+    }
+
+    printf("Connected to tracker sever %s:%d\n", tracker_address, tracker_port);
+
+    return sockid;
+}
 
 int main(int argc,char *argv[]) {
+    int tracker_port;
+    char tracker_address[16];
+    int  n_seconds;
+    int server_port;
 
-    printf("Hello World");
-/*
-    char server_address[50];
-    int server_port=3490;  // you should instead read from configuration file
-    
-   
-    struct sockaddr_in server_addr;
-    int sockid;
-        
-    if ((sockid = socket(AF_INET,SOCK_STREAM,0))==-1)\{//create socket
-        printf("socket cannot be created\\n"); exit(0);
-    }
-                                              
-   
-    server_addr.sin_family = AF_INET;//host byte order
-    server_addr.sin_port = htons(server_port);// convert to network byte order
-    if (connect(sockid ,(struct sockaddr *) &server_addr,sizeof(struct sockaddr))==-1)\{//connect and error check
-        printf("Cannot connect to server\\n"); exit(0);
+    read_client_thread_config(&tracker_port, tracker_address, &n_seconds);
+    read_server_thread_config(&server_port);
+
+    int tracker_sock = connect_tracker_server(tracker_address, tracker_port);
+
+    if(fork() == 0){
+        start_server(server_port);
     }
 
-   // If connected successfully
-    
-    if(!strcmp(argv[1],"list"))\{// if this is LIST command
-        int list_req=htons(LIST);
-        if((write(sockid,(char *)&list_req,sizeof(list_req))) < 0)\{//inform the server of the list request
-            printf("Send_request  failure\\n"); exit(0);
+    char line[MAXLINE];
+    while(true){
+        printf(">");
+        fflush(stdout);
+        if(fgets(line, MAXLINE, stdin) != NULL){
+            char* newline = strchr(line, '\n');
+            *newline = '\0';
+            handle_command(line, tracker_sock);
         }
-
-        if((read(sockid,(char *)&msg,sizeof(msg)))< 0)\{// read what server has said
-            printf("Read  failure\\n"); exit(0); 
-        }
-        
-        close(sockid);
-        printf("Connection closed\\n");
-        exit(0);
-    }//end close
-*/    
-}         
+    }
+}
